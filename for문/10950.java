86de55e4a6ea50fd03749b205e3d43dd723f3a6e
young <84887974+youngzziny@users.noreply.github.com>
2021-09-24 15:50:12 +0900

    10950 A+B - 3

---
+++ for문/10950.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for(int i=0; i<t; i++) {
+			System.out.println(sc.nextInt() + sc.nextInt());
+		}
+	}
+}
