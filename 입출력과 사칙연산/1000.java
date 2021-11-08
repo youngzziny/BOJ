37bb9eb3e7c5f414f617d4e744a8b986c10699fb
Zoey Young <kiki.laboratory@gmail.com>
2021-11-08 22:55:27 +0900

    1000 A+B

---
+++ 입출력과 사칙연산/1000.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+    
+        Scanner kb = new Scanner(System.in);
+        
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+        
+        kb.close();
+        
+        System.out.print(a+b);
+        
+    }
+}
