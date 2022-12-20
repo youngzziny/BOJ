c49c5ce536d1506ff1278f7834ae2bd1ffa6a049
youngzziny <kiki.laboratory@gmail.com>
2022-12-20 20:34:41 +0900

    3003 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ 입출력과 사칙연산/3003.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int k = 1 - sc.nextInt();
+	    int q = 1 - sc.nextInt();
+	    int r = 2 - sc.nextInt();
+	    int b = 2 - sc.nextInt();
+	    int kn = 2 - sc.nextInt();
+	    int p = 8 - sc.nextInt();
+	    
+	    System.out.println(k + " " + q + " " + r + " " + b + " " + kn + " " + p);
+	    
+	}
+}
