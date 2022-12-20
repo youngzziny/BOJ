817bb6ceea9c9dc872c33b42a88cd8d140fbb107
youngzziny <kiki.laboratory@gmail.com>
2022-12-20 20:25:03 +0900

    18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ 입출력과 사칙연산/18108.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+	    Scanner sc = new Scanner(System.in);
+	    int year = sc.nextInt();
+	    
+	    year -= 543;
+	    
+	    System.out.println(year);
+	    
+	}
+}
